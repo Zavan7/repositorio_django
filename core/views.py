7c8a4abde1c3d606f3e27754a906558abc68d463
Vitor Zavan <vitorzavanj@gmail.com>
2026-05-18 20:24:43 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'core/home.html')
+
+
+def sobre(request):
+    return render(request, 'core/sobre.html')
+
+
+def contato(request):
+    return render(request, 'core/contato.html')
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from portifolio-zavan!")
+
+
+if __name__ == "__main__":
+    main()
